d9961d36c7d1222039e0d20adceb7be337122175
Joaquín Sáez Sáez <juako.ss0303@gmail.com>
2025-09-02 14:52:36 -0400

    cambio en el main

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const { app, BrowserWindow } = require('electron')
+const path = require("path");
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+        nodeIntegration: true
+    }
     })
 
-    win.loadFile('index.html')
+    win.loadFile(path.join(__dirname,'view' ,'index.html'))
 }
 
 app.whenReady().then(() => {
